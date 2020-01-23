5d421602e65e355cd3fa1edc98bd077e381e4b2c
Jerzy Foss <jerzy.n.foss@durham.ac.uk>
2020-01-23 12:28:08 +0000

    Update data model/validation

---
+++ server/src/controllers/AuthController.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+
+class AuthController {
+	
+	login(req, resp) {
+		req.accepts('json');
+		resp.set('etag', false);
+		const user = {};
+		resp.cookie('_id', createAuth(user), {maxAge: config.auth.maxAge, httpOnly: true, secure: config.auth.secureCookie}); 
+	}
+	
+	createAuth(user){
+		const payload = {
+			id:user.id,
+			access:user.access
+		};
+		let token = jwt.sign(payload, config.auth.jwtSecret, {
+			expiresIn: config.auth.maxAge
+		});
+		return token;
+	}
+}
--- server/src/models/Validator.js
+++ server/src/models/Validator.js
@@ -1,6 +1,7 @@
 class Validator {
-	constructor(){
+	constructor(resource = null){
 		this.errors = [];
+		this.resource = resource;
 	}
 	
 	err(resource, field, validator, message){
@@ -8,41 +9,64 @@ class Validator {
 		return false;
 	}
 	
-	isNotNull(id, value, resource){
+	isNotNull(id, value){
 		if(value === undefined || value === null){
-			return this.err(resource, id, {'name':'null'}, resource + ' ' + id + ' must be completed.');
+			return this.err(this.resource, id, {'name':'null'}, this.resource + ' ' + id + ' must be completed.');
 		}
 		return true;
 	}
 	
-	isTime(id, value, resource){
+	isTime(id, value){
 		if(isNaN(new Date(value).getUTCMilliseconds())){
-			return this.err(resource, id, {'name':'time', 'format':'Y-m-d H:i:s'}, resource + ' ' + id + ' must be a valid time.');
+			return this.err(this.resource, id, {'name':'time', 'format':'Y-m-d H:i:s'}, this.resource + ' ' + id + ' must be a valid time.');
 		}
 		return true;
 	}
 	
-	isValidLength(id, value, resource, minLength, maxLength){
+	isValidLength(id, value, minLength, maxLength){
 		if(value.length > maxLength){
-			return this.err(resource, id, {'name':'length', 'max':maxLength, 'min':minLength}, resource + ' ' + id + ' must be at most ' + maxLength + ' characters long.');
+			return this.err(this.resource, id, {'name':'length', 'max':maxLength, 'min':minLength}, this.resource + ' ' + id + ' must be at most ' + maxLength + ' characters long.');
 		}
 		else if(value.length < minLength){
-			return this.err(resource, id, {'name':'length', 'max':maxLength, 'min':minLength}, resource + ' ' + id + ' must be at least ' + minLength + ' characters long.');
+			return this.err(this.resource, id, {'name':'length', 'max':maxLength, 'min':minLength}, this.resource + ' ' + id + ' must be at least ' + minLength + ' characters long.');
 		}
 		return true;
 	}
 	
-	isInRange(id, value, resource, min, max){
+	isInRange(id, value, min, max){
 		if(value > max){
-			return this.err(resource, id, {'name':'range', 'max':max, 'min':min}, resource + ' ' + id + ' must be less than or equal to ' + max + '.'});
+			return this.err(this.resource, id, {'name':'range', 'max':max, 'min':min}, this.resource + ' ' + id + ' must be less than or equal to ' + max + '.');
 		}
 		else if(value < min){
-			return this.err(resource, id, {'name':'range', 'max':max, 'min':min}, resource + ' ' + id + ' must be greater than or equal to ' + min + '.'});
+			return this.err(this.resource, id, {'name':'range', 'max':max, 'min':min}, this.resource + ' ' + id + ' must be greater than or equal to ' + min + '.');
 		}
 		return true;
 	}
 	
-	hasErrors(){
+	isBoolean(id, value) {
+		if (value !== true && value !== false) {
+			return this.err(this.resource, id, {'name':'isBoolean'}, this.resource + ' ' + id + ' must be a true or false value.');
+		}
+		return true;
+	}
+	
+	isTimeNN(id, value) {
+		return this.isNotNull(id, value) && this.isTime(id, value);
+	}
+	
+	isValidLengthNN(id, value, minLength, maxLength) {
+		return this.isNotNull(id, value) && this.isValidLength(id, value, minLength, maxLength);
+	}
+	
+	isInRangeNN(id, value, min, max) {
+		return this.isNotNull(id, value) && this.isInRange(id, value, min, max);
+	}
+	
+	isBooleanNN(id, value, min, max) {
+		return this.isNotNull(id, value) && this.isBoolean(id, value);
+	}
+	
+	hasErrors() {
 		return this.errors.length > 0;
 	}
 }
