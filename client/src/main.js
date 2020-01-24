5efccfe6f43bd070507c3bcf67e9b982a3c850c0
chqc66 <44363392+chqc66@users.noreply.github.com>
2020-01-24 22:12:05 +0000

    Added warehouseconfig.html

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,16 @@
+var app = new Vue({
+  el: '#test',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
 
+var addZone = new Vue({
+	el:'#newZone',
+	data:{
+		zone_name: '',
+		bays: 1,
+		columns:4,
+		rows:3
+	}
+})
