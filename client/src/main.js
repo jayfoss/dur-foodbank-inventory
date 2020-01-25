83666564159768dc9ab876a0f0bcef8db32c371b
chqc66 <44363392+chqc66@users.noreply.github.com>
2020-01-25 01:51:43 +0000

    Add an example report and page for userconfig

--- client/src/main.js
+++ client/src/main.js
@@ -14,3 +14,17 @@ var addZone = new Vue({
 		rows:3
 	}
 })
+
+var userData = new Vue({
+	el:'#user-data',
+	data:{
+		users:[
+		{userid:'abc123', permission:'admin'},
+		{userid:'dcs0zz', permission:'part timer'},
+		{userid:'abcdefg', permission:'part timer'},
+		{userid:'qwerty', permission:'part timer'},
+		{userid:'poiuy', permission:'part timer'},
+		{userid:'zxcvbn', permission:'part timer'}
+		]
+	}
+})
