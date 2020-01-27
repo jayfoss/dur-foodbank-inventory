749daade5c8c758fa8cf0f3e80d5d134343b7739
Aidan-WW <44363391+Aidan-WW@users.noreply.github.com>
2020-01-27 18:21:45 +0000

    added functionality
    is it working?

--- client/login-page-with-VueJS/login.vue
+++ client/login-page-with-VueJS/login.vue
@@ -24,6 +24,18 @@
                     if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
                         this.$emit("authenticated", true);
                         this.$router.replace({ name: "secure" });
+                        let currentObj = this;
+                        this.axious.post('http://localhose:8080/api/v1/auth', {
+                            email: this.username,
+                            password: this.password
+                        })
+                        .then(function (response){
+                            currentObj.output = response.data;
+                        })
+                        .catch(function (error){
+                            currentObj.output = error;
+                        });
+                        }
                     } else {
                         console.log("The username and / or password is incorrect");
                     }
