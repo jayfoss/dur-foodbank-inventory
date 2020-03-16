3c65d28d2e54d4fee216b506ec984dd9520a4f88
JF (Durham) <44465055+jfdur@users.noreply.github.com>
2020-03-16 22:38:19 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,22 @@ const development = {
 };
 
 const production = {
-	env: env
+	env: env,
+	app: {
+		port: process.env.port,
+		host: 'localhost'
+	},
+	auth: {
+		jwtSecret: '0d6581d4b2584100ee759e1cea29dcd353ae8dfa41785a293e6541c6b364adcee3e91e8e660c2f5602ff1a84373f9b89213e4f32806bbf5eafc6e6e8f1a85e42',
+		secureCookie: true,
+		maxAge: 3600
+	},
+	db: {
+		host: 'localhost',
+		port: 27017,
+		username: 'example@example.com',
+		password: 'Test'
+	}
 };
 
 const config = {
